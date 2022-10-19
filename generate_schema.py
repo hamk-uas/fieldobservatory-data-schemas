fea686e3e28001214a91fe48dfa268eb38d28caf
Olli Niemitalo <olli4@hamk.fi>
2022-10-19 11:53:26 +0300

    titles and title translations, expand hidden

--- generate_schema.py
+++ generate_schema.py
@@ -22,15 +22,24 @@ def get_choices(key, value):
         else:
             choices = csv[csv['category'] == value['choices']]['code_name']
         ret_val = {
+            "title": code_name_to_disp_name_eng[key] if key in code_name_to_disp_name_eng else 'unknown',
+            "title_en": code_name_to_disp_name_eng[key] if key in code_name_to_disp_name_eng else 'unknown',
+            "title_fi": code_name_to_disp_name_fin[key] if key in code_name_to_disp_name_fin else 'unknown',
             "type": "string",
             "oneOf": []
         }           
         for choice in choices:
             ret_val["oneOf"].append({
-                "const": choice
+                "const": choice,
+                "title": code_name_to_disp_name_eng[choice] if choice in code_name_to_disp_name_eng else 'unknown',
+                "title_en": code_name_to_disp_name_eng[choice] if choice in code_name_to_disp_name_eng else 'unknown',
+                "title_fi": code_name_to_disp_name_fin[choice] if choice in code_name_to_disp_name_fin else 'unknown'
             })
     else:
-        ret_val = {
+        ret_val = {    
+            "title": code_name_to_disp_name_eng[key] if key in code_name_to_disp_name_eng else 'unknown',
+            "title_en": code_name_to_disp_name_eng[key] if key in code_name_to_disp_name_eng else 'unknown',
+            "title_fi": code_name_to_disp_name_fin[key] if key in code_name_to_disp_name_fin else 'unknown',
             "type": "string"
         }
     return ret_val
@@ -42,48 +51,83 @@ schema = {
     "oneOf" : [
     ]
 }
+
+variable_names = csv #csv[csv['category'] == 'variable_name']
+code_name_to_disp_name_eng = {}
+code_name_to_disp_name_fin = {}
+for index, row in variable_names.iterrows():
+    code_name_to_disp_name_eng[row['code_name']] = row['disp_name_eng']
+    code_name_to_disp_name_fin[row['code_name']] = row['disp_name_fin']
+
 categories = csv[csv['category'] == 'mgmt_operations_event_choice']
+
 #print("categories:")
 #print(categories)
+
+# Go through different choices for mgmt_operations_event, listed in the display names CSV
 for index, row in categories.iterrows():
+    # (index is not needed)
+    # row is for example:
+    # category,code_name,disp_name_eng,disp_name_fin
+    # mgmt_operations_event_choice,planting,sowing,kylvö
+    #
+    # Create a sub-schema for this particular event type
     sub_schema = {
         'id': f"#{row['code_name']}",
-        'properties': {
+        'title': row['disp_name_eng'],
+        'title_en': row['disp_name_eng'],
+        'title_fi': row['disp_name_fin'],
+        'properties': { # Include some properties required for all event types
             "mgmt_operations_event": {
+                "title": code_name_to_disp_name_eng["mgmt_operations_event"] if "mgmt_operations_event" in code_name_to_disp_name_eng else 'unknown',
+                "title_en": code_name_to_disp_name_eng["mgmt_operations_event"] if "mgmt_operations_event" in code_name_to_disp_name_eng else 'unknown',
+                "title_fi": code_name_to_disp_name_fin["mgmt_operations_event"] if "mgmt_operations_event" in code_name_to_disp_name_fin else 'unknown',
                 "type": "string",
                 "const": row['code_name']
             },
             "date": {
+                "title": code_name_to_disp_name_eng["date"] if "date" in code_name_to_disp_name_eng else 'unknown',
+                "title_en": code_name_to_disp_name_eng["date"] if "date" in code_name_to_disp_name_eng else 'unknown',
+                "title_fi": code_name_to_disp_name_fin["date"] if "date" in code_name_to_disp_name_fin else 'unknown',
                 "type": "string",
                 "format": "date"
             },
             "mgmt_event_notes": {
+                "title": code_name_to_disp_name_eng["mgmt_event_notes"] if "mgmt_event_notes" in code_name_to_disp_name_eng else 'unknown',
+                "title_en": code_name_to_disp_name_eng["mgmt_event_notes"] if "mgmt_event_notes" in code_name_to_disp_name_eng else 'unknown',
+                "title_fi": code_name_to_disp_name_fin["mgmt_event_notes"] if "mgmt_event_notes" in code_name_to_disp_name_fin else 'unknown',
                 "type": "string"
             }
         },
         'type': 'object',
-        "required": ['mgmt_operations_event', 'date']
+        "required": ['mgmt_operations_event', 'date'] # This list may be expanded further down
     }
+    # Get the object describing this event type from the sub_elements list of mgmt_operations_event
     sub_element = ui_structure['form']['mgmt_operations_event']['sub_elements'][row['code_name']]
+    # Loop through the properties of the event type specific object
     for sub_element_key, sub_element_value in sub_element.items():
         if "code_name" in sub_element_value:
-            property = sub_element_value["code_name"]
-            #if "choices" in sub_element_value:
-                #print(sub_element_key)
-                #print(sub_element_value)
-                #chemical_applic_method
-                #{'code_name': 'chemical_applic_method', 'type': 'selectInput', 'label': 'chemical_applic_method_label', 'choices': 'FEACD'}                #print("choices available for:")
+            # If a property has a code_name then it will be a property in the JSON schema
+            property = sub_element_value["code_name"] # Get property identifier
+            # Parse choices for the property
             sub_schema["properties"][property] = get_choices(sub_element_key, sub_element_value)
-            if "sub_elements" in sub_element_value:
-                if "single" in sub_element_value["sub_elements"]:
-                    for element_of_single_key, element_of_single_value in sub_element_value["sub_elements"]["single"].items():
+            # If the event's property has a property "multiple": true, then it may have a
+            # property "sub_elements", which introduces additional dependent properties
+            if "multiple" in sub_element_value and sub_element_value["multiple"] and "sub_elements" in sub_element_value:
+                # "single" in "sub_elements" introduces new properties always present
+                sub_elements = sub_element_value["sub_elements"]
+                if "single" in sub_elements:
+                    for element_of_single_key, element_of_single_value in sub_elements["single"].items():
                         if "code_name" in element_of_single_value:
-                            #print(element_of_single_key)
-                            #print(element_of_single_value)
-                            # harvest_cut_height
-                            # {'code_name': 'harvest_cut_height', 'type': 'numericInput', 'label': 'harvest_cut_height_label', 'min': 0}
                             sub_schema["properties"][element_of_single_key] = get_choices(element_of_single_key, element_of_single_value)
+                # "hidden" in "sub_elements" introduces new properties that MAY be present
+                # Let's add them all anyhow
+                if "hidden" in sub_elements:
+                    for element_of_hidden_key, element_of_hidden_value in sub_elements["hidden"].items():
+                        if "code_name" in element_of_hidden_value:
+                            sub_schema["properties"][element_of_hidden_key] = get_choices(element_of_hidden_key, element_of_hidden_value)
+
     schema['oneOf'].append(sub_schema)
 
 f = open("management-event.schema.json", mode='w')
-print(json.dumps(schema, indent=4), file=f)
+print(json.dumps(schema, indent=4, ensure_ascii=True).encode('ascii').decode('unicode-escape'), file=f)
